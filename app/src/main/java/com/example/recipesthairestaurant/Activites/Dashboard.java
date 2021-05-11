d09a97cae0c3246858f6da1cda290acea4f22a4c
Manal Al-Subaei <alsubaeimanal@gmail.com>
2021-05-11 19:55:34 +0300

    The first screen "Splash Screen" finish

--- app/src/main/java/com/example/recipesthairestaurant/Activites/Dashboard.java
+++ app/src/main/java/com/example/recipesthairestaurant/Activites/Dashboard.java
@@ -1,11 +1,22 @@
 package com.example.recipesthairestaurant.Activites;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.recipesthairestaurant.R;
 
 public class Dashboard extends AppCompatActivity {
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            finishAndRemoveTask();
+        }
+        this.finishAffinity();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
